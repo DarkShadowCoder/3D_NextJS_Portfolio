2d172fdbe2e0e4cbec47a7f087fa8d046e1999c8
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2025-01-15 13:55:12 +0100

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -142,7 +142,7 @@ export const testimonials = [
   {
     quote:
       "Collaborating with Yvan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Yvan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Yvan is the ideal partner.",
-    name: "Abel Diakite",
+    name: "Abdel Diakite",
     title: "CEO Clever's startup",
   },
   {
